d5a8e1e2a24930ce6edb30cd723b4f467abc5cc0
nithinhasher <93312240+nithinhasher@users.noreply.github.com>
2023-09-06 19:25:33 +0530

    Update and rename Pipeline.groovy to pipeline.groovy

---
+++ vars/pipeline.groovy
@@ -0,0 +1,31 @@
+// package org.example
+
+// class Pipeline {
+def callClone(repoUrl) {
+    // Your clone logic here
+    sh "git clone ${repoUrl}"
+}
+
+// def callBuild(targetDir) {
+//     // Your build logic here
+//     dir(targetDir) {
+//         sh "mvn clean install"
+//     }
+// }
+
+// def callRunTests(targetDir) {
+//     // Your test logic here
+//     dir(targetDir) {
+//         sh "mvn test"
+//     }
+// }
+
+    // def callPushChanges(targetDir) {
+    //     // Your push changes logic here
+    //     dir(targetDir) {
+    //         sh "git add ."
+    //         sh "git commit -m 'Automated build and test'"
+    //         sh "git push"
+    //     }
+    // }
+// }
