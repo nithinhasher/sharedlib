8a054fa98a139d6ae28476515f4e2ad830287dfa
nithinhasher <93312240+nithinhasher@users.noreply.github.com>
2023-09-12 14:41:26 +0530

    Update checkoutCode.groovy

--- vars/checkoutCode.groovy
+++ vars/checkoutCode.groovy
@@ -1,6 +1,5 @@
-def call(String repoUrl, String branch){
-   def workingDir = "${env.WORKSPACE}"
-   sh "git clone ${repoUrl} ${workingDir}"
-   sh "git checkout ${branch}"
-   return workingDir
+def call(String repoUrl){
+   sh "git clone ${repoUrl}"
+   // sh "git checkout ${branch}"
+   // return workingDir
 }
