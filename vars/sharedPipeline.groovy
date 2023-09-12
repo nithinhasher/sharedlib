6c6a7682bd8f62a0e5184137e2162811d6578e13
nithinhasher <93312240+nithinhasher@users.noreply.github.com>
2023-09-12 14:58:25 +0530

    Update sharedPipeline.groovy

--- vars/sharedPipeline.groovy
+++ vars/sharedPipeline.groovy
@@ -3,14 +3,14 @@ def cloneRepo(String repoUrl){
    // sh "git checkout ${branch}"
    // return workingDir
 }
-def callname(name){
-  echo "Hey ${name}, How are you!!.."
-}
-// def callBuild(targetDir) {
-//     // Your build logic here
-//     dir(targetDir) {
-//         sh "mvn clean install"
-//     }
+// def callname(name){
+//   echo "Hey ${name}, How are you!!.."
 // }
+def callBuild(targetDir) {
+    // Your build logic here
+    dir(targetDir) {
+        sh "mvn clean install"
+    }
+}
 
 
