069dd969da89a4abdd3c8dce3bb53fffd6bd7af5
nithinhasher <93312240+nithinhasher@users.noreply.github.com>
2023-09-12 14:56:26 +0530

    Update and rename checkoutCode.groovy to sharedPipeline.groovy

--- vars/checkoutCode.groovy
+++
@@ -1,8 +0,0 @@
-def call(String repoUrl){
-   sh "git clone ${repoUrl}"
-   // sh "git checkout ${branch}"
-   // return workingDir
-}
-def callname(name){
-  echo "Hey ${name}, How are you!!.."
-}
---
+++ vars/sharedPipeline.groovy
@@ -0,0 +1,16 @@
+def cloneRepo(String repoUrl){
+   sh "git clone ${repoUrl}"
+   // sh "git checkout ${branch}"
+   // return workingDir
+}
+def callname(name){
+  echo "Hey ${name}, How are you!!.."
+}
+// def callBuild(targetDir) {
+//     // Your build logic here
+//     dir(targetDir) {
+//         sh "mvn clean install"
+//     }
+// }
+
+
